d02bf1623ff1b2f7d153e80abb2e0224fce8c2a1
h4nzdev <hanzhmagbal@gmail.com>
2026-08-01 17:27:45 +0800

    fix max width ui

--- Officials/src/pages/WasteAnalytics.jsx
+++ Officials/src/pages/WasteAnalytics.jsx
@@ -59,7 +59,7 @@ export default function WasteAnalytics() {
   const topHotspot = hotspots.length > 0 ? hotspots[0] : null;
 
   return (
-    <div className="p-6 max-w-7xl mx-auto space-y-6">
+    <div className="p-6 mx-auto space-y-6">
       {/* Header */}
       <div className="flex items-center justify-between">
         <div>
